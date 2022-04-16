8d59196cc5c2769bee46e722c7e676d3e1c3b42b
jSuttipong <senasuttiphan.s@gmail.com>
2022-04-16 15:01:03 +0700

    Add package

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -39,6 +40,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    'bootstrap-vue/nuxt'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
