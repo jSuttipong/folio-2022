e30569384dac28575d245a0feb760ea5b30da105
jSuttipong <senasuttiphan.s@gmail.com>
2022-05-02 14:44:45 +0700

    00f711e

---
+++ _nuxt/538a712.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{352:function(n,e,t){"use strict";t.r(e);var c={asyncData:function(n){return(0,n.redirect)("/")}},d=t(51),component=Object(d.a)(c,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
